7d44791952e4724d07125a5efecb850207a06bbe
KERJI <kirill.i.chirkov.06@gmail.com>
2026-07-08 21:31:59 +0300

    add new function in display

--- src/display.cpp
+++ src/display.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
-#include <LCD_1602_RUS_ALL.h>
+#include <LiquidCrystal_I2C.h>
 
 #include "state.h"
 #include "display.h"
 
-LCD_1602_RUS lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void display_init() {
     lcd.init();
@@ -20,11 +20,60 @@ void display_update() {
         break;
     
     case MODE_PREVIEW:
-    lcd.backlight();
+        lcd.backlight();
+
+        break;
+
+    case MODE_MENU:
+
+        break;
+
+    case MODE_CEILING:
+
+        break;
+
+    case MODE_CEILING_RED:
+
+        break;
+
+    case MODE_CEILING_GREEN:
+
+        break;
+        
+    case MODE_CEILING_BLUE:
+
+        break;
+
+    case MODE_MONITOR:
+
+        break;
+
+    case MODE_MONITOR_BRIGHTNESS:
+
+        break;
+
+    case MODE_MONITOR_COLOR:
     
+        break;
 
     default:
         break;
     }
 
+}
+
+void show_preview(MonitorState monitor_state, int monitor_brightness, int ceiling_brightness, RGB ceiling_color) {
+    lcd.backlight();
+    lcd.cursor_off();
+    lcd.setCursor(0, 0);
+    lcd.print("MS: ");
+    if (monitor_state == MONITOR_OFF) {
+        lcd.println("OFF");
+    } else if (monitor_state == MONITOR_A) {
+        lcd.println("BLUE");
+    } else if (monitor_state == MONITOR_B) {
+        lcd.println("WHITE");
+    }
+    lcd.setCursor(0, 1);
+    lcd.print("CEILING: ")
 }
--- src/display.h
+++ src/display.h
@@ -3,3 +3,5 @@
 void display_update();
 
 void display_init();
+
+void show_preview(MonitorState monitor_state, int monitor_brightness, int ceiling_brightness, RGB ceiling_color);
